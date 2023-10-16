d742fa5435a95b0de824e855afd19153fe92dce3
manuelpalomeque <pjonathanmanuel@hotmail.com>
2023-10-16 20:23:45 -0300

    Importamos librerias necesarias

---
+++ scrapy_16102023_v01.py
@@ -0,0 +1,6 @@
+# Para este proyecto, buscamos extraer los datos de los productos y del precio del mismo de la pagina web de Hiper Libertad:
+# https://www.hiperlibertad.com.ar/
+
+# 01 - Importamos las librerias necesarias:
+import scrapy
+from datetime import datetime
