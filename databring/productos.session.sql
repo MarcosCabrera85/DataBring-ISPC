c4bc7527f54a9b2da5aa6106f35bc23dca32f692
MarcosCabrera85 <133060733+MarcosCabrera85@users.noreply.github.com>
2023-10-20 16:36:32 -0600

    Add files via upload

---
+++ databring/productos.session.sql
@@ -0,0 +1,17 @@
+CREATE TABLE productos(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    marca VARCHAR(50),
+    precio VARCHAR(50)
+)
+
+CREATE TABLE ciudad (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50)
+
+)
+
+
+
+
+
